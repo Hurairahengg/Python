ada51b4d26d8381dd9bd360d3fdbe6511c6bf127
Hurairahengg <69149863+Hurairahengg@users.noreply.github.com>
2023-03-19 20:10:42 +0600

    Python Variables
    All the python variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+flot = 3.4 #A float variable
+intt = 4 #An integer variable
+name = 'Hurairah' #This is a string variable
+
+print(flot) #prints the float number
+print(intt) #prints the int 
+print(name) #prints the string
