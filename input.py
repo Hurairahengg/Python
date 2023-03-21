f60e8c82f61bc19bd17de8a8a96b9083b690ac71
Hurairahengg <69149863+Hurairahengg@users.noreply.github.com>
2023-03-21 19:14:10 +0600

    Input from user in python
    this code is an example of how you can take input from user and prints them

---
+++ input.py
@@ -0,0 +1,5 @@
+name = input('whats your name  ') #taking input
+favcolor = input('whats your fav color ') #taking input
+
+print('hello' + name) #prints 1 input
+print(favcolor + ' likes ' + name  )#prints both inputs from user
