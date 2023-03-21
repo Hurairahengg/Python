04cf65d72162ef4f5bd2ce4fcf68a8a6abd1bd0b
Hurairahengg <69149863+Hurairahengg@users.noreply.github.com>
2023-03-21 19:54:06 +0600

    Convert Functions
    This is an example of how to change types of variables

---
+++ TypeConvert.py
@@ -0,0 +1,6 @@
+born = input('which year did you born in- ') #taking input of age
+age = 2023 - int(born) #finding the age
+
+print(age) #printing the age
+print(type(age)) #printing the type
+print(type(born)) #printing the type
