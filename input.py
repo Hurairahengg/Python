f07b6543f79a0746326451655d0a9bf50c1f3488
Hurairah <69149863+Hurairahengg@users.noreply.github.com>
2023-03-21 19:42:24 +0600

    Update input.py

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
 name = input('whats your name  ') #taking input
 favcolor = input('whats your fav color ') #taking input
 
-print('hello' + name) #prints 1 input
-print(favcolor + ' likes ' + name  )#prints both inputs from user
+print('Hello' + name) #prints 1 input
+print(name + ' likes ' + favcolor  )#prints both inputs from user
